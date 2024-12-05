c95bacc7e685d6d9a50c5e8cb276655deaee8c86
DAO36 <mlpfimytl8st@gmail.com>
2024-12-04 22:03:18 -0500

    dddd

--- data/items.ts
+++ data/items.ts
@@ -45,6 +45,19 @@ export const Items: import('../sim/dex-items').ItemDataTable = {
         num: 669,
         gen: 6,
 	},
+    charmeleonite: {
+        name: "Charmeleonite",
+        spritenum: 607,
+        megaStone: "Charmeleon-Mega",
+        megaEvolves: "Charmeleon",
+        itemUser: ["Charmeleon"],
+        onTakeItem(item, source) {
+            if (item.megaEvolves === source.baseSpecies.baseSpecies) return false;
+            return true;
+        },
+        num: 669,
+        gen: 6,
+	},
 	absolite: {
 		name: "Absolite",
 		spritenum: 576,
