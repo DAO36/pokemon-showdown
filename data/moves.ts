af16424eb5479dd6f8c2ab964d951a5d942c373c
DAO36 <mlpfimytl8st@gmail.com>
2025-01-18 21:18:00 -0500

    gg

--- data/moves.ts
+++ data/moves.ts
@@ -2059,7 +2059,7 @@ export const Moves: import('../sim/dex-moves').MoveDataTable = {
 		name: "Mother Nature",
 		pp: 10,
 		priority: 0,
-		flags: {protect: 1, contact: 1, mirror: 1},
+		flags: {protect: 1, mirror: 1},
 		self: {
 			onHit(pokemon, source, move) {
 				this.add('-activate', source, 'move: Aromatherapy');
