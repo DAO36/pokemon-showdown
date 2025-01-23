204f5bd64fa3ce378fd6ae58832eba787c39b004
DAO36 <mlpfimytl8st@gmail.com>
2025-01-23 13:06:50 -0500

    ugh

--- data/abilities.ts
+++ data/abilities.ts
@@ -551,11 +551,34 @@ export const Abilities: import('../sim/dex-abilities').AbilityDataTable = {
 				this.add('-block', target, 'ability: Yandere', move, pokemon);
 				return null;
 			}
+			if (move.target === 'foeSide' || (move.target === 'all' && (move.flags['switches']))) {
+				this.add('-block', target, 'ability: Yandere', move, pokemon);
+				return null;
+			}
 		},
 		flags: {breakable: 1},
 		name: "Yandere",
 		rating: 3,
 		num: 171,
+	},
+	zling: {
+		onFoeTryMove(target, source, move) {
+			const targetAllExceptions = ['perishsong', 'flowershield', 'rototiller'];
+			if (move.target === 'foeSide' || (move.target === 'all' && !targetAllExceptions.includes(move.id))) {
+				return;
+			}
+
+			const dazzlingHolder = this.effectState.target;
+			if ((source.isAlly(dazzlingHolder) || move.target === 'all') && move.priority > 0.1) {
+				this.attrLastMove('[still]');
+				this.add('cant', dazzlingHolder, 'ability: Dazzling', move, '[of] ' + target);
+				return false;
+			}
+		},
+		flags: {breakable: 1},
+		name: "Dazzling",
+		rating: 2.5,
+		num: 219,
 	},
 	elvishflare: { // reskin of Rocky Payload but for Fire
 		onModifyAtkPriority: 5,
