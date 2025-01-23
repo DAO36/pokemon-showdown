66a7cb2252467f1c3943343144c5bfb6794bb7e5
DAO36 <mlpfimytl8st@gmail.com>
2025-01-22 23:30:19 -0500

    fdgfdgfdgfd

--- data/abilities.ts
+++ data/abilities.ts
@@ -548,14 +548,14 @@ export const Abilities: import('../sim/dex-abilities').AbilityDataTable = {
 	yandere: {
 		onFoeTryMove(target, source, move) {
 			const targetAllExceptions = ['revivalblessing', 'necromancy'];
-			if (move.target === 'foeSide' || (move.target === 'self' && move.id === 'teleport' && move.id === 'batonpass' && move.id === 'rainshaman' && move.id === 'chillyreception' && !targetAllExceptions.includes(move.id))) {
+			if (move.target === 'foeSide' || (move.target === 'all' || move.target === 'self' && move.id === 'teleport' && move.id === 'batonpass' && move.id === 'rainshaman' && move.id === 'chillyreception' && !targetAllExceptions.includes(move.id))) {
 				return;
 			}
 
 			const yandereHolder = this.effectState.target;
-			if ((source.isAlly(yandereHolder) || move.target === 'all') && move.flags['switches']) {
+			if ((source.isAlly(yandereHolder) || move.target === 'self') && move.flags['switches']) {
 				this.attrLastMove('[still]');
-				this.add('cant', yandereHolder, '[from]ability: Yandere', move, '[of] ' + target);
+				this.add('cant', yandereHolder, 'ability: Yandere', move, '[of] ' + target);
 				return false;
 			}
 		},
@@ -563,20 +563,6 @@ export const Abilities: import('../sim/dex-abilities').AbilityDataTable = {
 		name: "Yandere",
 		rating: 2.5,
 		num: 219,
-	},
-	mogu: { // reskin of Water Absorb but for Grass
-		onTryHit(target, source, move) {
-			if (target !== source && move.type === 'Grass') {
-				if (!this.heal(target.baseMaxhp / 4, target, target)) {
-					this.add('-immune', target, '[from] ability: Mogu Mogu');
-				}
-				return null;
-			}
-		},
-		flags: {breakable: 1},
-		name: "Mogu Mogu",
-		rating: 2.5,
-		num: 11,
 	},
 	elvishflare: { // reskin of Rocky Payload but for Fire
 		onModifyAtkPriority: 5,
