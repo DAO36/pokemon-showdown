697c8c7ac679c1ee59ea254ce3d54efc3463c817
DAO36 <mlpfimytl8st@gmail.com>
2025-01-23 01:17:15 -0500

    asdf

--- data/abilities.ts
+++ data/abilities.ts
@@ -546,9 +546,9 @@ export const Abilities: import('../sim/dex-abilities').AbilityDataTable = {
 		num: 89,
 	},
 	yandere: {
-		onFoeTryMove(target, source, move) {
+		onFoeTryMove(pokemon, target, move) {
 			if (move.flags['switches']) {
-				this.add('cant', target, target, 'ability: Yandere', move, '[from] ' + source);
+				this.add('cant', target, 'ability: Yandere', move, '[from] ' + pokemon);
 				return null;
 			}
 		},
