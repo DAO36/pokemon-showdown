2aa5cbb3f19b63da552b484e8758120186499f39
DAO36 <mlpfimytl8st@gmail.com>
2025-01-24 15:21:30 -0500

    uh

--- data/abilities.ts
+++ data/abilities.ts
@@ -562,12 +562,12 @@ export const Abilities: import('../sim/dex-abilities').AbilityDataTable = {
 			const yandereHolder = this.effectState.source;
 			this.attrLastMove('[still]');
 			if (move.flags['switches']) {
-				this.add('cant', pokemon, 'ability: Yandere', move, '[from] ' + source);
+				this.add('cant', source, 'ability: Yandere', move, '[from] ' + pokemon);
 				return null;
 			} 
 			if (move.id === 'teleport' || move.id === 'batonpass') {
 				this.attrLastMove('[still]');
-				this.add('cant', pokemon, 'ability: Yandere', move, '[from] ' + source);
+				this.add('cant', source, 'ability: Yandere', move, '[from] ' + pokemon);
 				return null;
 			}
 		},
