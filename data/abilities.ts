546424928f5915649a463e3e40d40216005f8efb
DAO36 <mlpfimytl8st@gmail.com>
2025-01-24 01:55:48 -0500

    sadsddsfsdsd

--- data/abilities.ts
+++ data/abilities.ts
@@ -554,7 +554,7 @@ export const Abilities: import('../sim/dex-abilities').AbilityDataTable = {
 			const yandereHolder = this.effectState.target;
 			if (move.id === 'teleport' || move.id === 'batonpass') {
 				this.attrLastMove('[still]');
-				this.add('-cant', target, 'ability: Yandere', move, '[of] ' + yandereHolder);
+				this.add('cant', target, 'ability: Yandere', move, '[of] ' + yandereHolder);
 				return false;
 			}
 		},
@@ -562,25 +562,6 @@ export const Abilities: import('../sim/dex-abilities').AbilityDataTable = {
 		name: "Yandere",
 		rating: 3,
 		num: 171,
-	},
-	zling: {
-		onFoeTryMove(target, source, move) {
-			const targetAllExceptions = ['perishsong', 'flowershield', 'rototiller'];
-			if (move.target === 'foeSide' || (move.target === 'all' && !targetAllExceptions.includes(move.id))) {
-				return;
-			}
-
-			const dazzlingHolder = this.effectState.target;
-			if ((source.isAlly(dazzlingHolder) || move.target === 'all') && move.priority > 0.1) {
-				this.attrLastMove('[still]');
-				this.add('cant', dazzlingHolder, 'ability: Dazzling', move, '[of] ' + target);
-				return false;
-			}
-		},
-		flags: {breakable: 1},
-		name: "Dazzling",
-		rating: 2.5,
-		num: 219,
 	},
 	test: {
 		onFoeTryMove(pokemon, target, move) {
