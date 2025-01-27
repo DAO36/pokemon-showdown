48d6395ad5bf2721c386506d34f6abcbe839be24
DAO36 <mlpfimytl8st@gmail.com>
2025-01-27 18:44:45 -0500

    ok

--- data/abilities.ts
+++ data/abilities.ts
@@ -824,13 +824,15 @@ export const Abilities: import('../sim/dex-abilities').AbilityDataTable = {
 			for (const targetCondition of removeTarget) {
 				if (target.side.removeSideCondition(targetCondition)) {
 					if (!removeAll.includes(targetCondition)) continue;
-					this.add('-sideend', target.side, this.dex.conditions.get(targetCondition).name, 'ability: Blow Away', '[of] ' + target);
+					this.add('-activate', target, 'ability: Cleaner');
+					this.add('-sideend', target.side, this.dex.conditions.get(targetCondition).name);
 					success = true;
 				}
 			}
 			for (const sideCondition of removeAll) {
 				if (source.side.removeSideCondition(sideCondition)) {
-					this.add('-sideend', source.side, this.dex.conditions.get(sideCondition).name, 'ability: Blow Away', '[of] ' + target);
+					this.add('-activate', target, 'ability: Cleaner');
+					this.add('-sideend', source.side, this.dex.conditions.get(sideCondition).name);
 					success = true;
 				}
 			}
