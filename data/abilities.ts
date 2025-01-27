1cc2626771f24ae016ae955d880f3c8ab172e2c9
DAO36 <mlpfimytl8st@gmail.com>
2025-01-26 20:58:05 -0500

    uhhhh

--- data/abilities.ts
+++ data/abilities.ts
@@ -826,6 +826,7 @@ export const Abilities: import('../sim/dex-abilities').AbilityDataTable = {
 				const removeAll = [
 					'reflect', 'lightscreen', 'auroraveil', 'hologram', 'mist', 'spikes', 'toxicspikes', 'stealthrock', 'stickyweb', 'gmaxsteelsurge',
 				];
+				for (const side of [pokemon.side, ...pokemon.side.foeSidesWithConditions()])
 				for (const remove of removeAll)  
 					if (pokemon.side.removeSideCondition(remove))  
 						if (!success)   
