9c06b0aa21b79f9f4df6c16e74c148d3a785b4b0
DAO36 <mlpfimytl8st@gmail.com>
2025-02-07 04:55:29 -0500

    set

--- data/abilities.ts
+++ data/abilities.ts
@@ -209,7 +209,7 @@ export const Abilities: import('../sim/dex-abilities').AbilityDataTable = {
 	},
 	stellar: { // combines [Moxie] + [Curious Medicine] but better this.add('-activate', pokemon, 'ability: Shuba Shuba'); 
 		onStart(pokemon) {
-			this.add('-clearallboost', pokemon, 'ability: Stellar');
+			this.add('-clearallboost', pokemon, '[from] ability: Stellar');
 			for (const pokemon of this.getAllPokemon()) {
 				pokemon.clearBoosts();
 			}
