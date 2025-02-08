8e7f1ae09592b0c45a653e33b6d749b52f30ce21
DAO36 <mlpfimytl8st@gmail.com>
2025-02-07 21:59:30 -0500

    uh?

--- data/abilities.ts
+++ data/abilities.ts
@@ -99,7 +99,7 @@ export const Abilities: import('../sim/dex-abilities').AbilityDataTable = {
 		onAfterMoveSecondary(target, source, move) {
 			if (target.hp <= target.maxhp / 2) {
 				if (!source.isActive || !this.canSwitch(source.side) || source.forceSwitchFlag || target.forceSwitchFlag) {
-					return; 
+					this.add("-activate", target, 'ability: Survivalist'); 
 				}
 				// The item is used up even against a pokemon with Ingrain or that otherwise can't be forced out
 				if (target.hasAbility('gtfo')) {
@@ -180,7 +180,7 @@ export const Abilities: import('../sim/dex-abilities').AbilityDataTable = {
 			}
 			if (activate && pokemon.hasAbility('downbad')) {
 				pokemon.setBoost(boosts);
-				this.add('-clearnegativeboost', pokemon, 'ability: Down Bad');
+				this.add('-clearnegativeboost', pokemon, '[from] ability: Down Bad', '[of] ' + pokemon);
 			}
 		},
 		flags: {},
