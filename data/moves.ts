a1113e69f923de8419c16dfd2c7934deaefe07a3
DAO36 <mlpfimytl8st@gmail.com>
2025-02-09 12:55:05 -0500

    uh

--- data/moves.ts
+++ data/moves.ts
@@ -2758,10 +2758,8 @@ export const Moves: import('../sim/dex-moves').MoveDataTable = {
 		pp: 5,
 		priority: 0,
 		flags: {protect: 1, mirror: 1, pulse: 1}, 
-		onModifyMove(move, pokemon, target) {
-			if (this.field.getPseudoWeather('gravity') && pokemon.isGrounded()) { 
-				move.accuracy = 80; 
-			}
+		onModifyMove(move) {
+			if (this.field.getPseudoWeather('gravity')) move.accuracy = 80;
 		},
 		secondary: {
 			chance: 100,
