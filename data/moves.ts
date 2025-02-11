0fab78a90905f5820bf1f670581b6359bd2fcbee
DAO36 <mlpfimytl8st@gmail.com>
2025-02-11 16:58:03 -0500

    bruhhhhhhhh

--- data/moves.ts
+++ data/moves.ts
@@ -3624,7 +3624,7 @@ export const Moves: import('../sim/dex-moves').MoveDataTable = {
 		name: "Fisted",
 		pp: 5,
 		priority: 0,
-		flags: {contact: 1, protect: 1, mirror: 1, punch: 1},
+		flags: {contact: 1, mirror: 1, punch: 1},
 		onTryHit(pokemon) {
 			// will shatter screens through sub, before you hit
 			pokemon.side.removeSideCondition('reflect');
