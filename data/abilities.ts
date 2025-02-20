b5aed397609e2ea8ad4f3cc04ff7af0d2947624b
DAO36 <mlpfimytl8st@gmail.com>
2025-02-19 23:40:07 -0500

    uhh

--- data/abilities.ts
+++ data/abilities.ts
@@ -1926,9 +1926,9 @@ export const Abilities: import('../sim/dex-abilities').AbilityDataTable = {
 				this.add('-block', target, 'ability: Mother Nature', '[of] ' + effectHolder);
 			}
 		},   
-		onResidualOrder: 5,
-		onResidualSubOrder: 3,
 		onResidual(pokemon) {
+			let success = false;
+			const allies = [...pokemon.side.pokemon, ...pokemon.side.allySide?.pokemon || []]; 
 			for (const allyActive of pokemon.adjacentAllies()) {
 				if (allyActive.status) {
 					this.add('-activate', pokemon, 'ability: Mother Nature');
