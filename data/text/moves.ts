48cb391075098fa21a34865f479f420d5de32334
DAO36 <mlpfimytl8st@gmail.com>
2025-10-10 19:38:49 -0400

    ssrb typo

--- data/text/moves.ts
+++ data/text/moves.ts
@@ -8059,7 +8059,7 @@ export const MovesText: {[id: IDEntry]: MoveText} = {
 	},
 	ssrb: {
 		name: "SSRB",
-		desc: "Hits ten times. This move checks accuracy for each hit, and the attack ends if the target avoids a hit. If one of the hits breaks the target's substitute, it will take damage for the remaining hits. If the user has the Skill Link Ability, this move will always hit ten time.",
+		desc: "Hits ten times. This move checks accuracy for each hit, and the attack ends if the target avoids a hit. If one of the hits breaks the target's substitute, it will take damage for the remaining hits. If the user has the Skill Link Ability, this move will always hit ten times.",
 		shortDesc: "Hits 10 times. Each hit can miss.",
 	},
 	lalion: {
