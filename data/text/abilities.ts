51d707d03930202a9cf87dada00f98c287d21cb5
DAO36 <mlpfimytl8st@gmail.com>
2026-05-07 15:44:17 -0400

    r
    r

--- data/text/abilities.ts
+++ data/text/abilities.ts
@@ -507,7 +507,7 @@ export const AbilitiesText: { [id: IDEntry]: AbilityText } = {
 		desc: "On switch-in, this Pokemon's Defense or Special Defense is raised by 1 stage based on the weaker combined offensive stat of all opposing Pokemon. Physical Defense is raised by 1 if their Physical Attack is higher, and Special Defense is raised by 1 if their Special Attack is the same or higher.",
 		shortDesc: "On switch-in, Defense or Sp.Def raises by 1 based on foes' weaker Offensive stat.",
 	},
-	mammamia: {
+	mammamia: { // RAORA
 		name: "Mamma Mia",
 		desc: "If this Pokemon eats a Berry, they will eat that Berry again at the end of the turn. If the last item this Pokemon used is a Berry, there is a 25% chance it gets restored at the end of each turn.",
 		shortDesc: "User eats a Berry: eats Berry again. If item used is a Berry, 25% chance to restore.",
