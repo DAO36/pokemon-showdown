b31403fd3a40e2d9fd8c944fa96d6f03a24579e7
DAO36 <mlpfimytl8st@gmail.com>
2026-05-12 00:35:10 -0400

    uyhhu
    hu

--- data/abilities.ts
+++ data/abilities.ts
@@ -2579,6 +2579,7 @@ export const Abilities: import('../sim/dex-abilities').AbilityDataTable = {
 	investment: { // STOMPING TANTRUM but an ability
 		onModifyMove(move, pokemon, target) {
 			if (pokemon.moveLastTurnResult === false) {
+				this.debug('doubling BP due to previous move failure');
 				return move.basePower * 2;
 			}
 		},
