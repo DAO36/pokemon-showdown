2db9a73614f036ee5541c478a738fe8990b50744
DAO36 <mlpfimytl8st@gmail.com>
2026-05-15 05:20:49 -0400

    aaaaaa
    aaa

--- data/abilities.ts
+++ data/abilities.ts
@@ -2634,12 +2634,13 @@ export const Abilities: import('../sim/dex-abilities').AbilityDataTable = {
 		name: "Bancho",
 		rating: 2,
 		num: 240,
-	}, // if (!target || target.fainted) return;
+	},
 	mangaka: { // COLOR CHANGE but in reverse
 		onAfterMove(source, target, move) {
             if (move.hasBounced || move.flags['futuremove'] || move.sourceEffect === 'snatch') return;
             const type = move.type;
-            if (move.category !== 'Status' && type && type !== '???' && target.getTypes().join() !== type && !target.fainted && target.apparentType > '3') {
+            if (move.category !== 'Status' && type && type !== '???' && target.getTypes().join() !== type && !target.fainted) {
+				if (move.type === 'Normal' && target.apparentType === 'Ghost')
                 if (!target.setType(type)) return;
 				this.add('-activate', source, 'ability: Mangaka');
                 this.add('-start', target, 'typechange', type);
