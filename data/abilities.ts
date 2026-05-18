66e78cdbce98fd1e8b9e9c4ae7a3de618cbfea9d
DAO36 <mlpfimytl8st@gmail.com>
2026-05-18 03:32:00 -0400

    werfwerfwe
    weffwefew

--- data/abilities.ts
+++ data/abilities.ts
@@ -741,6 +741,7 @@ export const Abilities: import('../sim/dex-abilities').AbilityDataTable = {
 	haachamacooking: { // FLINCH on switch in - HEALS when using STATUS moves
 		onStart(pokemon) {
             let activated = false;
+			const foe = pokemon.side.foe.active[pokemon.side.active.length - 1 - pokemon.position]
             for (const target of pokemon.adjacentFoes()) {
                 if (!activated) {
                     this.add('-ability', pokemon, 'Haachama Cooking', 'boost');
@@ -1067,6 +1068,7 @@ export const Abilities: import('../sim/dex-abilities').AbilityDataTable = {
 		},
 		onStart(pokemon) {
             let activated = false;
+			const foe = pokemon.side.foe.active[pokemon.side.active.length - 1 - pokemon.position]
             for (const target of pokemon.adjacentFoes()) {
                 if (!activated) {
                     this.add('-ability', pokemon, 'Peko Peko', 'boost');
