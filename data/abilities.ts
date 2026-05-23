1aae51b8ff04ada8c82f66cf996a5d37ef2ebf9e
DAO36 <mlpfimytl8st@gmail.com>
2026-05-22 21:06:22 -0400

    wdwdw
    wdwd

--- data/abilities.ts
+++ data/abilities.ts
@@ -1733,7 +1733,7 @@ export const Abilities: import('../sim/dex-abilities').AbilityDataTable = {
 			const target = pokemon.adjacentFoes()[0];
 			const oldApparentType = pokemon.apparentType;
 			let newBaseTypes = target.getTypes(true).filter(type => type !== '???');
-			if (!target || target.fainted) return;
+			if (!target || target.fainted) return false;
 			if (!newBaseTypes.length) {
 				if (target.addedType) {
 					newBaseTypes = ['Normal'];
