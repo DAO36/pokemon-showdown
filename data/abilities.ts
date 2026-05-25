e9d9ffe80648048f7e426416e736c2c3ab86ec17
DAO36 <mlpfimytl8st@gmail.com>
2026-05-24 21:19:38 -0400

    f
    f

--- data/abilities.ts
+++ data/abilities.ts
@@ -1820,6 +1820,7 @@ export const Abilities: import('../sim/dex-abilities').AbilityDataTable = {
 			if (!target.knownType) target.apparentType = oldApparentType;
 		},
 		onSwitchIn(pokemon) {
+			if (this.effectState.target !== pokemon.side.foe.active[pokemon.side.foe.active.length - 1 - pokemon.position]) return;
 			const foe = pokemon.side.foe.active[pokemon.side.active.length - 1 - pokemon.position]
             const adjacentFoe = pokemon.adjacentFoes()[0]; 
             const oldApparentType = pokemon.apparentType;
