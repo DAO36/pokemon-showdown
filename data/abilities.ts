7389b26714f5333fbea5bbcc124988aa7667ce63
DAO36 <mlpfimytl8st@gmail.com>
2026-05-29 14:21:27 -0400

    d
    d

--- data/abilities.ts
+++ data/abilities.ts
@@ -50,8 +50,8 @@ export const Abilities: import('../sim/dex-abilities').AbilityDataTable = {
                 if (boost[i]! > 0) {
                     boostPlus[i] = (boostPlus[i] || 0) + boost[i]!;
                 }
-				const feaster = this.effectState.target;
 				this.attrLastMove('[still]');
+				this.add('cant', target, 'ability: Feast or Famine', effect, '[of] source');
 				return false;
             }
             target.clearBoosts();
