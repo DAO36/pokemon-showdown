0e311f637a0a54c1b6eb5f2e2d29d73b3506edee
DAO36 <mlpfimytl8st@gmail.com>
2026-05-28 23:15:55 -0400

    3
    3

--- data/abilities.ts
+++ data/abilities.ts
@@ -1671,7 +1671,7 @@ export const Abilities: import('../sim/dex-abilities').AbilityDataTable = {
 				return this.chainModify(1.5);
 			}
 		},
-		onWeather(target, source, effect) {
+		onResidual(target, source, effect) {
 			if (this.field.getPseudoWeather('gravity')) {
 				this.damage(target.baseMaxhp / 8, target, target);
 			}
@@ -1684,18 +1684,46 @@ export const Abilities: import('../sim/dex-abilities').AbilityDataTable = {
 	moonmight2: {
 		onModifySpAPriority: 5,
 		onModifySpA(spa, pokemon) {
-			if (['gravity'].includes(pokemon.effectiveWeather())) {
+			if (this.field.getPseudoWeather('gravity')) {
 				return this.chainModify(1.5);
 			}
 		},
-		onWeather(target, source, effect) {
-			if (target.effectiveWeather() !== effect.id) return;
-			if (effect.id === 'gravity') {
+		onModifyAtkPriority: 5,
+		onModifyAtk(spa, pokemon) {
+			if (this.field.getPseudoWeather('gravity')) {
+				return this.chainModify(1.5);
+			}
+		},
+		onUpdate(target) {
+			if (this.field.getPseudoWeather('gravity')) {
 				this.damage(target.baseMaxhp / 8, target, target);
 			}
 		},
 		flags: {},
 		name: "Moon Might2",
+		rating: 2,
+		num: 94,
+	},
+	moonmight3: {
+		onModifySpAPriority: 5,
+		onModifySpA(spa, pokemon) {
+			if (this.field.getPseudoWeather('gravity')) {
+				return this.chainModify(1.5);
+			}
+		},
+		onModifyAtkPriority: 5,
+		onModifyAtk(spa, pokemon) {
+			if (this.field.getPseudoWeather('gravity')) {
+				return this.chainModify(1.5);
+			}
+		},
+		onUpdate(pokemon) {
+			if (this.field.getPseudoWeather('gravity')) {
+				this.damage(pokemon.baseMaxhp / 8, pokemon, pokemon);
+			}
+		},
+		flags: {},
+		name: "Moon Might3",
 		rating: 2,
 		num: 94,
 	},
