cc72640ece948d5117a967c7766bf45075e3525e
DAO36 <mlpfimytl8st@gmail.com>
2026-05-31 03:32:03 -0400

    d
    d

--- data/abilities.ts
+++ data/abilities.ts
@@ -41,12 +41,42 @@ export const Abilities: import('../sim/dex-abilities').AbilityDataTable = {
 		num: 0,
 	},
 	missedme: { // STOMPING TANTRUM but as an ability
+		onFoeAfterMove(source, target, move) {
+			if (source.moveThisTurnResult === false)
+				this.boost({def: 1}, target);
+		},
+		flags: {breakable: 1},
+		name: "Missed Me",
+		rating: 2.5,
+		num: 11,
+	},
+	missedme2: { // STOMPING TANTRUM but as an ability
 		onFoeAfterMove(pokemon, target, move) {
 			if (target.moveThisTurnResult === false)
 				this.boost({def: 1}, pokemon);
 		},
 		flags: {breakable: 1},
-		name: "Missed Me",
+		name: "Missed Me2",
+		rating: 2.5,
+		num: 11,
+	},
+	missedme3: { // STOMPING TANTRUM but as an ability
+		onFoeAfterMove(pokemon, target, move) {
+			if (pokemon.moveThisTurnResult === false)
+				this.boost({def: 1}, target);
+		},
+		flags: {breakable: 1},
+		name: "Missed Me3",
+		rating: 2.5,
+		num: 11,
+	},
+	missedme4: { // STOMPING TANTRUM but as an ability
+		onFoeAfterMove(source, target, move) {
+			if (target.moveThisTurnResult === false)
+				this.boost({def: 1}, source);
+		},
+		flags: {breakable: 1},
+		name: "Missed Me4",
 		rating: 2.5,
 		num: 11,
 	},
