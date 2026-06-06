6cc48fbeb870c6519f1585a4f5f61f5f889c8082
DAO36 <mlpfimytl8st@gmail.com>
2026-06-06 04:10:31 -0400

    e
    e

--- data/abilities.ts
+++ data/abilities.ts
@@ -1219,6 +1219,23 @@ export const Abilities: import('../sim/dex-abilities').AbilityDataTable = {
 		rating: 0,
 		num: 294,
 	},
+	pportunist: {
+		onFoeAfterBoost(boost, target, source, effect) {
+			if (effect?.name === 'Opportunist' || effect?.name === 'Mirror Herb') return;
+			if (!this.effectState.boosts) this.effectState.boosts = {} as SparseBoostsTable;
+			const boostPlus = this.effectState.boosts;
+			let i: BoostID;
+			for (i in boost) {
+				if (boost[i]! > 0) {
+					boostPlus[i] = (boostPlus[i] || 0) + boost[i]!;
+				}
+			}
+		},
+		flags: {},
+		name: "Opportunist",
+		rating: 3,
+		num: 290,
+	},
 	piracy: {
         onFoeTryBoost(boost, target, source, effect) {
             if (effect?.name === 'Opportunist' || effect?.name === 'Mirror Herb')
@@ -1229,11 +1246,11 @@ export const Abilities: import('../sim/dex-abilities').AbilityDataTable = {
             let i: BoostID;
             for (i in boost) {
                 if (boost[i]! < 0)
-                    return
+                    return;
 				
-                if (boost[i]! > 0) 
+                if (boost[i]! > 0) {
                     boostPlus[i] = (boostPlus[i] || 0) + boost[i]!;
-                
+                }
                 const feaster = this.effectState.target
             }
             return false;
