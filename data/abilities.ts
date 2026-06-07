e6f9a965c676d27ab8a2bf0bc33d0bf55063725c
DAO36 <mlpfimytl8st@gmail.com>
2026-06-07 03:34:28 -0400

    uh
    h

--- data/abilities.ts
+++ data/abilities.ts
@@ -1252,14 +1252,14 @@ export const Abilities: import('../sim/dex-abilities').AbilityDataTable = {
                 return;
             if (!this.effectState.boosts)
                 this.effectState.boosts = {} as SparseBoostsTable;
-            const boostPlus = this.effectState.boosts < 0 ? this.effectState.boosts : {};
+            const boostPlus = this.effectState.boosts;
             let i: BoostID;
             for (i in boost) {
                 if (boost[i]! < 0)
                     return;
 				
                 if (boost[i]! > 0) {
-                    boostPlus[i] = (boostPlus[i] || 0) + boost[i]!;
+                    boostPlus[i] = boost[i]!;
                 }
                 const feaster = this.effectState.target
             }
