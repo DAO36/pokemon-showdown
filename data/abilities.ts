08b911bdae1627cdbea2e841c78dc192e3d5f5a3
DAO36 <mlpfimytl8st@gmail.com>
2026-06-06 22:06:58 -0400

    e
    e

--- data/abilities.ts
+++ data/abilities.ts
@@ -1256,14 +1256,14 @@ export const Abilities: import('../sim/dex-abilities').AbilityDataTable = {
             let i: BoostID;
             for (i in boost) {
                 if (boost[i]! < 0)
-                    return;
+                    this.add('-clearpositiveboost');
 				
                 if (boost[i]! > 0) {
                     boostPlus[i] = (boostPlus[i] || 0) + boost[i]!;
                 }
                 const feaster = this.effectState.target
             }
-            this.add('-clearpositiveboost', target);
+            return false;
         },
         onFoeTryBoost(boost, target, source, effect) {
             if (effect?.name === 'Opportunist' || effect?.name === 'Mirror Herb')
