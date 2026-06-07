178ea6dac90e202de7e798036e5776fd3da196ea
DAO36 <mlpfimytl8st@gmail.com>
2026-06-07 10:52:34 -0400

    uh
    h

--- data/abilities.ts
+++ data/abilities.ts
@@ -1248,22 +1248,16 @@ export const Abilities: import('../sim/dex-abilities').AbilityDataTable = {
 			this.add('-clearpositiveboost', foe);
 		},
         onFoeTryBoost(boost, target, source, effect) {
-            if (effect?.name === 'Opportunist' || effect?.name === 'Mirror Herb')
-                return;
-            if (!this.effectState.boosts)
-                this.effectState.boosts = {} as SparseBoostsTable;
+            if (effect?.name === 'Opportunist' || effect?.name === 'Mirror Herb') return; // TODO Add this ability in case you have a mirror match
+            if (!this.effectState.boosts) this.effectState.boosts = {} as SparseBoostsTable;
             const boostPlus = this.effectState.boosts;
             let i: BoostID;
             for (i in boost) {
-                if (boost[i]! < 0)
-                    return;
-				
                 if (boost[i]! > 0) {
                     boostPlus[i] = (boostPlus[i] || 0) + boost[i]!;
+                    delete boost[i];
                 }
-                const feaster = this.effectState.target
             }
-            return false;
         },
         onAnySwitchInPriority: -3,
         onAnySwitchIn() {
