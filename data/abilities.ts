fdc38a1639485bc1596576b10d8784d0145d2f09
DAO36 <mlpfimytl8st@gmail.com>
2026-06-06 22:12:46 -0400

    d
    d

--- data/abilities.ts
+++ data/abilities.ts
@@ -1256,10 +1256,10 @@ export const Abilities: import('../sim/dex-abilities').AbilityDataTable = {
             let i: BoostID;
             for (i in boost) {
                 if (boost[i]! < 0)
+                    this.add('-clearpositiveboost');
 				
                 if (boost[i]! > 0) {
                     boostPlus[i] = (boostPlus[i] || 0) + boost[i]!;
-					this.add('-clearpositiveboost');
                 }
                 const feaster = this.effectState.target
             }
