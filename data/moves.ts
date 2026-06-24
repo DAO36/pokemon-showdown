108447f0cda39ff2974a3a6be57ccf7619bd4b0a
DAO36 <mlpfimytl8st@gmail.com>
2026-06-24 01:16:30 -0400

    f
    f

--- data/moves.ts
+++ data/moves.ts
@@ -2664,50 +2664,6 @@ export const Moves: import('../sim/dex-moves').MoveDataTable = {
 		type: "Dark",
 		contestType: "Clever",
 	},
-	wickedblows: {
-		num: 918,
-		accuracy: 100,
-		basePower: 120,
-		category: "Physical",
-		name: "Wicked Blows",
-		pp: 15,
-		priority: 4,
-		flags: { contact: 1, bypasssub: 1, mirror: 1, punch: 1 },
-		onTry(source, move, target) {
-			if (!move.id === 'protect') {
-				return false;
-			}
-		},
-		secondary: {
-			chance: 100,
-			volatileStatus: 'flinch',
-		},
-		target: "normal",
-		type: "Dark",
-	},
-	perhand: {
-		num: 918,
-		accuracy: 100,
-		basePower: 65,
-		category: "Physical",
-		name: "Upper Hand",
-		pp: 15,
-		priority: 3,
-		flags: { contact: 1, protect: 1, mirror: 1, metronome: 1 },
-		onTry(source, target) {
-			const action = this.queue.willMove(target);
-			const move = action?.choice === 'move' ? action.move : null;
-			if (!move || move.priority <= 0.1 || move.category === 'Status') {
-				return false;
-			}
-		},
-		secondary: {
-			chance: 100,
-			volatileStatus: 'flinch',
-		},
-		target: "normal",
-		type: "Fighting",
-	},
 	v7strike: { // ZETA 2
 		num: 364,
 		accuracy: 100,
